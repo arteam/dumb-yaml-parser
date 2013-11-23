bf688d96a483695c6315203bb14e7bc39464b59f
Artem Prigoda <arteamon@gmail.com>
2013-11-23 17:55:17 +0400

    Date Converter

---
+++ src/test/java/test/data/DateInterval.java
@@ -0,0 +1,45 @@
+package test.data;
+
+import annotation.DateConverter;
+import annotation.Name;
+
+import java.util.Date;
+
+/**
+ * Date: 11/23/13
+ * Time: 5:47 PM
+ *
+ * @author Artem Prigoda
+ */
+public class DateInterval {
+
+    private final Date startDate;
+    private final Date endDate;
+
+    public DateInterval(@Name("start_date") @DateConverter("yyyy-MM-dd HH:mm") Date startDate,
+                        @Name("end_date") @DateConverter("yyyy-MM-dd HH:mm") Date endDate) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DateInterval that = (DateInterval) o;
+
+        if (endDate != null ? !endDate.equals(that.endDate) : that.endDate != null) return false;
+        if (startDate != null ? !startDate.equals(that.startDate) : that.startDate != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "DateInterval{" +
+                "startDate=" + startDate +
+                ", endDate=" + endDate +
+                '}';
+    }
+}
